10e1aede9b17a91eba80af341926b3c3aa41779d
TomRiu <nguyenducanhtu12@gmail.com>
2024-10-09 16:09:09 +0700

    update: count likes, show time

--- src/components/Post/PostCard.jsx
+++ src/components/Post/PostCard.jsx
@@ -26,9 +26,11 @@ const PostCard = ({ item }) => {
   const dispatch = useDispatch();
   const { post, auth } = useSelector((store) => store);
   const [showComments, setShowComments] = useState(false);
+
   const handleShowComments = () => {
     setShowComments(!showComments);
   };
+
   const handleCreateComment = (content) => {
     const reqData = {
       postId: item.id,
@@ -42,8 +44,9 @@ const PostCard = ({ item }) => {
   const handleLikePost = () => {
     dispatch(likePostAction(item.id));
   };
+
   return (
-    <Card className="">
+    <Card>
       <CardHeader
         avatar={
           <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
@@ -56,15 +59,25 @@ const PostCard = ({ item }) => {
             <MoreVertIcon />
           </IconButton>
         }
-        title={item.user.firstName + " " + item.user.lastName}
-        subheader={item.user.email.toLowerCase()}
+        title={
+          <div className="flex items-center">
+            <span>{item.user.firstName + " " + item.user.lastName}</span>
+            <Typography
+              variant="body2"
+              color="text.secondary"
+              sx={{ marginLeft: 1 }}
+            >
+              {new Date(item.createdAt).toLocaleString()}
+            </Typography>
+          </div>
+        }
+        subheader={
+          <Typography variant="body2" color="text.secondary">
+            {item.user.email.toLowerCase()}
+          </Typography>
+        }
       />
       <CardMedia component="img" height="100" image={item.image} alt="" />
-      {/* <img
-        className="w-full max-h-[30rem] object-cover object-top"
-        src={item.image}
-        alt=""
-      /> */}
       <CardContent>
         <Typography variant="body2" sx={{ color: "text.secondary" }}>
           {item.caption}
@@ -72,7 +85,7 @@ const PostCard = ({ item }) => {
       </CardContent>
 
       <CardActions className="flex justify-between" disableSpacing>
-        <div>
+        <div className="flex items-center">
           <IconButton onClick={handleLikePost}>
             {isLikedByReqUser(auth.user.id, item) ? (
               <FavoriteIcon />
@@ -94,15 +107,19 @@ const PostCard = ({ item }) => {
         </div>
       </CardActions>
 
+      <Typography variant="body2" sx={{ marginLeft: 2, marginBottom: 1 }}>
+        {item.liked.length} {item.liked.length === 1 ? "Like" : "Likes"}
+      </Typography>
+
       {showComments && (
         <section>
           <div className="flex items-center space-x-5 mx-3 my-5">
             <Avatar sx={{}} />
             <input
               onKeyDown={(e) => {
-                if (e.key == "Enter") {
+                if (e.key === "Enter") {
                   handleCreateComment(e.target.value);
-                  console.log("Enter pressed -----", e.target.value);
+                  e.target.value = "";
                 }
               }}
               className="w-full outline-none bg-transparent border border-[#3b4054] rounded-full px-5 py-2"
@@ -112,7 +129,7 @@ const PostCard = ({ item }) => {
           </div>
           <div className="mx-3 space-y-2 my-5 text-xs">
             {item.comments?.map((comment) => (
-              <div className="flex items-center space-x-5">
+              <div className="flex items-center space-x-5" key={comment.id}>
                 <Avatar
                   sx={{ height: "2rem", width: "2rem", fontSize: ".8rem" }}
                 >
